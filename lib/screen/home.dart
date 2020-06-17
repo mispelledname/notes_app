070826c2ddb0292a88c1b8b460d4a3cf53dbda8a
Zi Ng <zyng@student.unimelb.edu.au>
2020-06-17 23:45:12 +1000

    add comments

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,14 +7,19 @@ import 'package:notesapp/widget/add_button.dart';
 import 'package:notesapp/screen/new_recording.dart';
 import 'package:notesapp/screen/mask.dart';
 
+/// Home Screen
+/// 
+/// Main Screen of the application. 
+/// 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+  // displays new recording screen if true
   bool _addingRecording; 
+  // displays new text note screen if true 
   bool _addingText; 
 
   void initState() {
@@ -23,21 +28,21 @@ class _HomeScreenState extends State<HomeScreen> {
     _addingText = false; 
   }
 
-  // open screen to add recording 
+  /// open screen to add recording 
   void addRecording() {
     setState(() {
       _addingRecording = !_addingRecording; 
     });
   }
 
-  // open screen to add recording 
+  /// open screen to add text note 
   void addTextNote() {
     setState(() {
       _addingText = !_addingText; 
     });
   }
 
-  // build new Recording Screen
+  /// build new Recording Screen
   Widget _buildNewRecording() {
     return new NewRecording(
       isHidden: !_addingRecording, 
@@ -45,16 +50,18 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // build the title and search bar 
+  /// build the title and search button
   Widget _buildTitle() {
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
+          // title
           Text(
             "Notes", 
             style: Theme.of(context).textTheme.headline1,
           ),
+          // search button
           Container(
             alignment: Alignment.centerRight,
             child: Container(
@@ -67,18 +74,23 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // build the mask 
+  /// Build the mask 
+  /// 
+  /// When use is adding a new recoding / text note, masking adds to user 
+  /// experience by adding contrast 
   Widget _buildMask(BuildContext context) {
     bool _displayMask = this._addingRecording || this._addingText;
     double opacity = _displayMask? Constants.maskOpacity2 : 0;
     return Mask(opacity: opacity);
   }
   
+  /// Home Screen Builder 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          // top row with title and search button
           Container(
             padding: EdgeInsets.only(left: 20, top: 60, right: 20),
             child: Column(
@@ -96,9 +108,9 @@ class _HomeScreenState extends State<HomeScreen> {
             addRecordingCallback: addRecording, 
             addTextCallback: addTextNote,
           ),
-          // Add recording
+          // Widget to add new recording 
           _buildNewRecording(),
-          // Add text note 
+          // Widget to dd text note 
         ]
       ),
     ); 
